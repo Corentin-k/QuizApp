5b184c19d1d889daa7892b02a1f3d6224d984f32
Corentin-k <c.kervagoret@outlook.fr>
2025-01-14 20:50:31 +0100

    update the database

--- backend/db.sql
+++ backend/db.sql
@@ -3,9 +3,8 @@ CREATE DATABASE IF NOT EXISTS quiz_db;
 
 USE quiz_db;
 
--- Table des joueurs
 CREATE TABLE users (
-                       id VARCHAR(36) PRIMARY KEY,  -- UUID for players
+                       id VARCHAR(36) PRIMARY KEY,
                        name VARCHAR(255) NOT NULL UNIQUE,
                        password VARCHAR(255) NOT NULL,
                        link VARCHAR(255) NOT NULL,
@@ -13,27 +12,28 @@ CREATE TABLE users (
                        role ENUM('admin', 'player') DEFAULT 'player'
 );
 
--- Table des questions
+
 CREATE TABLE questions (
                            id INT AUTO_INCREMENT PRIMARY KEY,
-                           question TEXT NOT NULL,
-                           answers JSON NOT NULL,  -- Store answers as a JSON array
-                           correct_answer INT NOT NULL
+                           type ENUM('song', 'qcm', 'question') NOT NULL,
+                           question TEXT,
+                           options JSON,
+                           correct_answer TEXT,
+                           metadata JSON DEFAULT NULL
 );
 
--- Table des réponses
 CREATE TABLE responses (
                            id INT AUTO_INCREMENT PRIMARY KEY,
-                           player_id VARCHAR(36) NOT NULL,  -- Reference to the user's id (UUID)
-                           question_id INT NOT NULL,  -- Reference to the questions table
-                           answer INT NOT NULL,
-                           FOREIGN KEY (player_id) REFERENCES users(id),  -- Reference to the users table
-                           FOREIGN KEY (question_id) REFERENCES questions(id)  -- Reference to the questions table
+                           player_id VARCHAR(36) NOT NULL,
+                           question_id INT NOT NULL,
+                           answer TEXT NOT NULL,
+                           FOREIGN KEY (player_id) REFERENCES users(id),
+                           FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
--- Insert sample questions into the questions table
-INSERT INTO questions (question, answers, correct_answer) VALUES
-                                                              ('Quelle est la capitale de la France ?', '["Paris", "Lyon", "Marseille", "Nice"]', 0),
-                                                              ('Combien font 5 + 3 ?', '["5", "8", "7", "10"]', 1);
+INSERT INTO questions (type, question, options, correct_answer) VALUES
+    ('qcm', 'Quelle est la capitale de la France ?', '["Paris", "Lyon", "Marseille", "Nice"]', '0');
+
+-- ! Creation of an admin user
 INSERT INTO users (id, name, password, link, role) VALUES
     ('550e8400-e29b-41d4-a716-446655440000', 'admin', '$2b$10$HzQ7P2Xv8jpYZ37/EE4JE.0JlE3iYQFFtcPztSKAPl8K3Mlecjvnu', 'admin', 'admin');
